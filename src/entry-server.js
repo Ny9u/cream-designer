41658403a4538e9cfca1c51a78d80bbfae5a23de
ny9u <604098901@qq.com>
2024-08-21 22:37:51 +0800

    SSR服务端渲染

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,12 +1,12 @@
 //服务端渲染入口
-import { createApplication } from "./main"
-import { renderToString } from "@vue/server-renderer"
+import { createApplication } from './main'
+import { renderToString } from '@vue/server-renderer'
 
-export async function render(){
-  const { app } = createApplication()
-  const ctx = {};
-  // renderToString将此时根实例转换成对应的HTML字符串
-  const html = await renderToString(app, ctx);
+export async function render() {
+	const { app } = createApplication()
+	const ctx = {}
+	// renderToString将此时根实例转换成对应的HTML字符串
+	const html = await renderToString(app, ctx)
 
-  return { html }
+	return { html }
 }
