d974a7b6e0eaa2b6495cbcfb7fe6ca3734b77330
ny9u <604098901@qq.com>
2024-12-08 16:38:01 +0800

    进一步优化打包结果

--- vite.config.js
+++ vite.config.js
@@ -3,80 +3,97 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url'
 import { Plugin as importToCdn ,autoComplete } from 'vite-plugin-cdn-import'
 import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig(({command,mode}) => {
   const isLibBuild = command === 'build' && mode === 'production';
   return{
-  plugins: [
-    vue(),
-    importToCdn({
-      modules: [
-        autoComplete('axios'),
-        {
-          name: 'vue',
-          var: 'Vue',
-          path: 'https://unpkg.com/vue@3.4.31/dist/vue.global.js'
-        },
-        {
-          name: 'element-plus',
-          var: 'ElementPlus',
-          path: 'https://unpkg.com/element-plus@2.7.7/dist/index.full.js',
-          css: 'https://unpkg.com/element-plus@2.7.7/dist/index.css'
-        },
-        {
-          name: '@element-plus/icons-vue',
-          var: 'ElementPlusIconsVue',
-          path: 'https://unpkg.com/@element-plus/icons-vue@2.3.1/dist/index.iife.min.js',
-        },
-        {
-          name:'ace-builds',
-          var: 'ace',
-          path: 'https://unpkg.com/ace-builds@1.35.4/src-noconflict/ace.js'
-        },
-        {
-          name:'js-beautify',
-          var: 'beautify',
-          path: 'https://unpkg.com/js-beautify@1.15.1/js/index.js'
-        },
-        {
-          name:'vuedraggable',
-          var:'draggable',
-          path: 'https://unpkg.com/vuedraggable@4.1.0/dist/vuedraggable.umd.min.js'
-        }
-      ]
-    }),
-    visualizer({
-      open: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+      vue(),
+      importToCdn({
+        modules: [
+          autoComplete('axios'),
+          {
+            name: 'vue',
+            var: 'Vue',
+            path: 'https://unpkg.com/vue@3.4.31/dist/vue.global.js'
+          },
+          {
+            name: 'element-plus',
+            var: 'ElementPlus',
+            path: 'https://unpkg.com/element-plus@2.7.7/dist/index.full.js',
+            css: 'https://unpkg.com/element-plus@2.7.7/dist/index.css'
+          },
+          {
+            name: '@element-plus/icons-vue',
+            var: 'ElementPlusIconsVue',
+            path: 'https://unpkg.com/@element-plus/icons-vue@2.3.1/dist/index.iife.min.js',
+          },
+          {
+            name:'ace-builds',
+            var: 'ace',
+            path: 'https://unpkg.com/ace-builds@1.35.4/src-noconflict/ace.js'
+          },
+          {
+            name:'js-beautify',
+            var: 'beautify',
+            path: 'https://unpkg.com/js-beautify@1.15.1/js/index.js'
+          },
+          {
+            name:'vuedraggable',
+            var:'draggable',
+            path: 'https://unpkg.com/vuedraggable@4.1.0/dist/vuedraggable.umd.min.js'
+          }
+        ]
+      }),
+      visualizer({
+        open: true,
+      }),
+      viteCompression({
+        verbose: true, 
+        disable: false,
+        threshold: 10240, 
+        algorithm: 'gzip', 
+        ext: '.gz',
+        deleteOriginFile: true 
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
     },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-  },
-  build: {
-    lib: isLibBuild ? { 
-      entry: './src/index.js', 
-      name: 'CreamDesigner', 
-      fileName: (format) => `cream-designer.${format}.js`, 
-    }: undefined,
-    outDir: isLibBuild ? 'dist/lib': 'dist', 
-    rollupOptions: {
-      external: [
-        'vue', 
-        'element-plus', 
-        '@element-plus/icons-vue',
-        'ace-builds',
-        'js-beautify',
-        'vuedraggable'
-      ],
-      output: {
-        globals: {
-          vue: 'Vue',
+    build: {
+      lib: isLibBuild ? { 
+        entry: './src/index.js', 
+        name: 'CreamDesigner', 
+        fileName: (format) => `cream-designer.${format}.js`, 
+      }: undefined,
+      outDir: isLibBuild ? 'dist/lib': 'dist', 
+      rollupOptions: {
+        external: [
+          'vue', 
+          'element-plus', 
+          '@element-plus/icons-vue',
+          'ace-builds',
+          'js-beautify',
+          'vuedraggable'
+        ],
+        output: {
+          globals: {
+            vue: 'Vue',
+          },
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          }
         },
       },
     },
-  }
+    esbuild: {
+      drop: ['console', 'debugger']
+    }
   }
 });
